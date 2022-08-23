0729a297f10d77fa5e2110f8745f548bd80e92ee
Niranjan halder <88826961+niranjan017294@users.noreply.github.com>
2022-08-23 10:43:42 +0600

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -4,7 +4,7 @@
     define('LOCALHOST', 'localhost');
     define('DB_USERNAME', 'nirusql');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'food-order');
+    define('DB_NAME', 'food_order');
 
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); // Database connection
     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); // Selecting Database
